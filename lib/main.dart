04a7a94ad59eccf8a5ba22a130fc217abc77ad81
Muli Job <munyoki912@gmail.com>
2024-05-10 12:13:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:crime_free/screens/signin_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Crime Reporting',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
